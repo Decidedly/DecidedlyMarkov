b6190697d77e6e35a5d6d0b15415827eadb35348
David Hilowitz <dhilowitz@gmail.com>
2015-02-10 21:52:17 -0500

    Fixed examples

--- examples/example1.php
+++ examples/example1.php
@@ -1,18 +1,18 @@
 <?php
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
-class Markov1 {
+class Example1 {
 	public function run() {
 		$taco  = new \Decidedly\Taco;
 		$markov = new \Decidedly\TextGenerators\SimpleMarkovGenerator();
 		$text = file_get_contents("alice.txt");
 		$markov->parseText($text);
-		print_r($markov->relations);
+		
 		$string = $markov->generateText(140);
 		echo $string . "\n";
 	}
 }
 
-$app = new Markov1();
+$app = new Example1();
 $app->run();
