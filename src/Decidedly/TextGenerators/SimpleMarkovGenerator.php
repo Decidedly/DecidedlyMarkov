fc92014c72b0a9a613e318fc573ace50842f34b7
David Hilowitz <dhilowitz@gmail.com>
2015-02-13 17:41:41 -0500

    Formatting
    Signed-off-by: David Hilowitz <dhilowitz@gmail.com>

--- src/Decidedly/TextGenerators/SimpleMarkovGenerator.php
+++ src/Decidedly/TextGenerators/SimpleMarkovGenerator.php
@@ -36,8 +36,8 @@ class SimpleMarkovGenerator {
 				array_shift($lastWords);
 			}
 		 
-		  // Get next word
-		  $thisWord = strtok($this->delim);
+			// Get next word
+			$thisWord = strtok($this->delim);
 		}
 	}
 
@@ -136,26 +136,26 @@ class SimpleMarkovGenerator {
 	}
 
 	/**
-   * getRandomWeightedElement()
-   * Utility function for getting random values with weighting.
-   * Pass in an associative array, such as array('A'=>5, 'B'=>45, 'C'=>50)
-   * An array like this means that "A" has a 5% chance of being selected, "B" 45%, and "C" 50%.
-   * The return value is the array key, A, B, or C in this case.  Note that the values assigned
-   * do not have to be percentages.  The values are simply relative to each other.  If one value
-   * weight was 2, and the other weight of 1, the value with the weight of 2 has about a 66%
-   * chance of being selected.  Also note that weights should be integers.
-   *
-   * @url http://stackoverflow.com/a/11872928/479540
-   * @param array $weightedValues
-   */
-  function getRandomWeightedElement(array $weightedValues) {
-    $rand = mt_rand(1, (int) array_sum($weightedValues));
+	 * getRandomWeightedElement()
+	 * Utility function for getting random values with weighting.
+	 * Pass in an associative array, such as array('A'=>5, 'B'=>45, 'C'=>50)
+	 * An array like this means that "A" has a 5% chance of being selected, "B" 45%, and "C" 50%.
+	 * The return value is the array key, A, B, or C in this case.  Note that the values assigned
+	 * do not have to be percentages.  The values are simply relative to each other.  If one value
+	 * weight was 2, and the other weight of 1, the value with the weight of 2 has about a 66%
+	 * chance of being selected.  Also note that weights should be integers.
+	 *
+	 * @url http://stackoverflow.com/a/11872928/479540
+	 * @param array $weightedValues
+	 */
+	function getRandomWeightedElement(array $weightedValues) {
+		$rand = mt_rand(1, (int) array_sum($weightedValues));
 
-    foreach ($weightedValues as $key => $value) {
-      $rand -= $value;
-      if ($rand <= 0) {
-        return $key;
-      }
-    }
-  }
+		foreach ($weightedValues as $key => $value) {
+			$rand -= $value;
+			if ($rand <= 0) {
+				return $key;
+			}
+		}
+	}
 }
